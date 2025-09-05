6d7e70adeb3c1e333b5df8408998bc41617c9d11
faheem Siddiqui <faheemsiddiqu786@gmail.com>
2025-09-05 17:38:00 +0530

    [new:]this is the my first commit

---
+++ src/main/java/com/siddiqui/ahmad/EnotesServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siddiqui.ahmad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesServiceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddiqui/ahmad/EnotesServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddiqui.ahmad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
